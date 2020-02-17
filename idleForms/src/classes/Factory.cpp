bd026b16011d32456c0f94ba5a1976c82a5b3dd7
salixe <salixe1214lol1214salixe@live.ca>
2020-02-17 18:21:13 -0500

    Adding Factory class files

---
+++ idleForms/src/classes/Factory.cpp
@@ -0,0 +1,8 @@
+//
+//  Factory.cpp
+//  idleForms
+//
+//  Created by Salixe St-Amand on 2020-02-17.
+//
+
+#include "Factory.h"
---
+++ idleForms/src/classes/Factory.h
@@ -0,0 +1,13 @@
+//
+//  Factory.hpp
+//  idleForms
+//
+//  Created by Salixe St-Amand on 2020-02-17.
+//
+
+#ifndef Factory_h
+#define Factory_h
+
+#include <stdio.h>
+
+#endif /* Factory_h */
