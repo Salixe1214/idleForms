bdb66779451d591532d5786c8e2f8e3a047115e5
salixe <salixe1214lol1214salixe@live.ca>
2020-02-17 18:25:07 -0500

    Adding Industry class files

---
+++ idleForms/src/classes/Industry.cpp
@@ -0,0 +1,8 @@
+//
+//  Industry.cpp
+//  idleForms
+//
+//  Created by Salixe St-Amand on 2020-02-17.
+//
+
+#include "Industry.hpp"
---
+++ idleForms/src/classes/Industry.hpp
@@ -0,0 +1,13 @@
+//
+//  Industry.hpp
+//  idleForms
+//
+//  Created by Salixe St-Amand on 2020-02-17.
+//
+
+#ifndef Industry_hpp
+#define Industry_hpp
+
+#include <stdio.h>
+
+#endif /* Industry_hpp */
